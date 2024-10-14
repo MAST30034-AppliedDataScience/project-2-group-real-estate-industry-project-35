35fe334534a54abf1c66b94dc56b2b07d8260f40
Leonia-W <lijialanw@student.unimelb.edu.au>
2024-10-15 00:51:34 +1100

    debug

--- notebooks/unzip_geopackage.py
+++ notebooks/unzip_geopackage.py
@@ -2,8 +2,8 @@
 import zipfile
 import os
 
-zip_file_path = "D:\STUDYfile\\ads2\\111ads\project-2-group-real-estate-industry-project-35\data\\raw\external\Geopackage.zip"
-extract_to_path = "D:\STUDYfile\\ads2\\111ads\project-2-group-real-estate-industry-project-35\data\\raw\external\extracted_files"
+zip_file_path = "../data/raw/external/Geopackage.zip"
+extract_to_path = "../data/raw/external/extracted_files"
 
 # Ensure the target directory exists
 os.makedirs(extract_to_path, exist_ok=True)
